2354416179c073e574a564e7494e8e353769119f
Ivan Mang <ivanmang1997@gmail.com>
2021-03-04 17:31:42 +0800

    Fixed the layout and padding problem

--- app/src/main/java/com/example/foodfinder/network/PlacesApiService.kt
+++ app/src/main/java/com/example/foodfinder/network/PlacesApiService.kt
@@ -1,22 +1,12 @@
 package com.example.foodfinder.network
 
-// import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
-import android.graphics.Bitmap
-import android.graphics.ImageFormat
-import android.media.Image
-import android.widget.ImageView
 import com.example.foodfinder.ApiResult
 import com.example.foodfinder.Constants
 import com.example.foodfinder.PlaceApiResult
-import com.example.foodfinder.PlaceDetail
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import org.json.JSONArray
-// import kotlinx.coroutines.Deferred
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
