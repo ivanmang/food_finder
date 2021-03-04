017a76b4d464418fccf3d933d814dce24c167561
Ivan Mang <ivanmang1997@gmail.com>
2021-03-04 12:00:14 +0800

    Order restaurant  list in desc order

--- app/src/main/java/com/example/foodfinder/Place.kt
+++ app/src/main/java/com/example/foodfinder/Place.kt
@@ -4,14 +4,16 @@ import android.location.Location
 import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverter
 import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 
 @Parcelize
 @Entity(tableName = "place_table")
 data class Place(
-    @PrimaryKey   val place_id: String,
-                  val name : String,
-                  val rating : String,
-                  val vicinity : String
-                  ) : Parcelable
+    @PrimaryKey val place_id: String,
+    val name: String,
+    val rating: String,
+    val vicinity: String,
+) : Parcelable
