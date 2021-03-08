feb18c00d01fd58471e8bcb5ff26c4541523d6ea
Ivan Mang <ivanmang1997@gmail.com>
2021-03-08 11:25:13 +0800

    Added like and dislike button

--- app/src/main/java/com/example/foodfinder/PlaceDetail.kt
+++ app/src/main/java/com/example/foodfinder/PlaceDetail.kt
@@ -4,5 +4,6 @@ package com.example.foodfinder
 data class PlaceDetail (
     val name : String,
     val photos : List<Photo> = ArrayList<Photo>(),
-    val geometry : Geometry
+    val geometry : Geometry,
+    val vicinity: String = ""
 )
